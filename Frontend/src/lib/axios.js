f47ef9141ac1659d0f2c28a7111edbaee8ff75ff
Rajesh Pal <ajoypal.raj@gmail.com>
2025-06-24 00:04:30 +0600

    base url is updated

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:5050/api/v1",
+    // baseURL:"http://localhost:5050/api/v1",
+    baseURL:"http://goalgrid1.onrender.com/api/v1",
     withCredentials:true,
 })
 
